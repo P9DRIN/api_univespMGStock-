0c5b8a16b6a0f10cb5a514a4ac2f394426833590
Lima Silva <pedrohenriquelimasilva2837@hotmail.com>
2024-04-24 19:05:51 -0300

    feat: Added cors to vercel

--- src/server.js
+++ src/server.js
@@ -10,5 +10,6 @@ app.use(cors());
 app.use(express.json());
 db.connect()
 app.use(routes)
+app.use(cors({ origin: 'https://univesp-mg-stock.vercel.app'}))
 
 app.listen(port, () => {console.log(`🚀 Backend started at http://localhost:${port} `)})
